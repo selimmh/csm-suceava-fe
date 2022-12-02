aaa2159b6e6d1c91cb6610e179d827e1f1835e4f
selimmh <selim.mht@gmail.com>
2022-12-02 12:26:47 +0200

    [develop]/code improvements

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+export { default as Login } from './Admin/Login'
+
+export { default as History } from './Details/History'
+export { default as Vision } from './Details/Vision'
+export { default as Trophies } from './Details/Trophies'
+
+export { default as Gallery } from './Gallery/Gallery'
+
+export { default as Home } from './Home/Home'
+
+export { default as Juniors } from './Juvenile/Juniors'
+export { default as Cadets } from './Juvenile/Cadets'
+export { default as MiniVolley } from './Juvenile/MiniVolley'
+export { default as Youth } from './Juvenile/Youth'
+
+export { default as Matches } from './Matches/Matches'
+
+export { default as News } from './News/News'
+export { default as New } from './News/New'
+
+export { default as NotFound } from './NotFound/NotFound'
+
+export { default as Partners } from './Partners/Partners'
+
+export { default as Coaches } from './Personnel/Coaches'
+export { default as Players } from './Personnel/Players'
+
+export { default as Seniors } from './Seniors/Seniors'
