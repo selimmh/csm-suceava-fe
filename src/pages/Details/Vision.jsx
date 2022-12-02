ea9dd2d61d2142e8f28a6aec224f750e84c82869
selimmh <selim.mht@gmail.com>
2022-12-02 12:25:08 +0200

    [develop]/add history and vision pages

---
+++ src/pages/Details/Vision.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import PageLabel from "../../components/Labels/PageLabel";
+
+const Vision = () => {
+  return (
+    <>
+      <PageLabel
+        label="Viziune"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, iste."
+      />
+      <div>
+        <img
+          className="w-full shadow-lg rounded-md border"
+          src={"https://picsum.photos/400/200"}
+          alt="Header"
+        />
+        <div className="mt-16">
+          <p className="indent-8 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            natus ipsa exercitationem odio, nam deserunt, quas quia eveniet
+            pariatur assumenda rem nostrum dolorum earum debitis eum provident
+            inventore architecto molestiae at, facere rerum sapiente. Placeat
+            optio alias aliquam itaque nesciunt ex adipisci nisi doloribus
+            reprehenderit quibusdam distinctio nobis eaque voluptatem corporis
+            voluptatibus earum, eum laboriosam commodi aliquid sequi voluptate.
+            Ipsam sit quod est eligendi porro sunt fugit magnam sint aut
+            reiciendis, consectetur eum tenetur quis explicabo officiis quo
+            similique maxime veniam. Soluta tenetur laborum praesentium quaerat
+            commodi. Mollitia, distinctio nisi repellendus, commodi suscipit,
+            enim illum numquam necessitatibus magni totam beatae!
+          </p>
+          <p className="indent-8 mb-4">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit,
+            et provident. Deleniti, rerum? Dolor inventore corporis,
+            repudiandae, ullam officia voluptas in iusto ea provident aliquam,
+            quidem quod possimus. Commodi itaque sapiente quo provident! Esse
+            impedit veritatis laudantium cumque quisquam id, aut dignissimos
+            nulla facere amet. Sapiente aut iusto perferendis quae blanditiis
+            distinctio fuga assumenda repellendus dolorem maxime eligendi,
+            corrupti in obcaecati beatae expedita debitis amet eaque fugit at
+            molestiae voluptatum? Cupiditate libero enim deleniti non unde
+            quaerat eos, corrupti, consequuntur saepe reiciendis explicabo ullam
+            qui? Fugiat assumenda nobis eaque aut adipisci atque cum veniam
+            temporibus enim recusandae? Ex qui quo aliquam quod? Ullam
+            laboriosam dolor accusantium ad alias provident aperiam aliquid.
+            Nihil doloremque fugit voluptatum asperiores quaerat totam quos
+            magni cupiditate. Hic similique repellendus cupiditate voluptatem
+            suscipit harum magni ab praesentium perferendis deserunt assumenda
+            laborum quam neque, corporis sit aspernatur explicabo distinctio
+            consequatur vitae quisquam rem totam fugit ea accusamus. Ratione
+            odio et laboriosam placeat, nobis facilis quasi quia a dolorem
+            ducimus quaerat dolores? Porro asperiores minus a nulla eaque! Illum
+            aperiam eligendi illo perferendis placeat, corrupti, magni quas
+            soluta quis molestiae a adipisci necessitatibus distinctio! Ipsa,
+            inventore totam impedit, delectus laborum velit animi harum voluptas
+            distinctio reprehenderit quaerat mollitia.
+          </p>
+
+          <p className="indent-8 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            natus ipsa exercitationem odio, nam deserunt, quas quia eveniet
+            pariatur assumenda rem nostrum dolorum earum debitis eum provident
+            inventore architecto molestiae at, facere rerum sapiente. Placeat
+            optio alias aliquam itaque nesciunt ex adipisci nisi doloribus
+            reprehenderit quibusdam distinctio nobis eaque voluptatem corporis
+            voluptatibus earum, eum laboriosam commodi aliquid sequi voluptate.
+            Ipsam sit quod est eligendi porro sunt fugit magnam sint aut
+            reiciendis, consectetur eum tenetur quis explicabo officiis quo
+            similique maxime veniam. Soluta tenetur laborum praesentium quaerat
+            commodi. Mollitia, distinctio nisi repellendus, commodi suscipit,
+            enim illum numquam necessitatibus magni totam beatae!
+          </p>
+
+          <p className="indent-8 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            natus ipsa exercitationem odio, nam deserunt, quas quia eveniet
+            pariatur assumenda rem nostrum dolorum earum debitis eum provident
+            inventore architecto molestiae at, facere rerum sapiente. Placeat
+            optio alias aliquam itaque nesciunt ex adipisci nisi doloribus
+            reprehenderit quibusdam distinctio nobis eaque voluptatem corporis
+            voluptatibus earum, eum laboriosam commodi aliquid sequi voluptate.
+            Ipsam sit quod est eligendi porro sunt fugit magnam sint aut
+            reiciendis, consectetur eum tenetur quis explicabo officiis quo
+            similique maxime veniam. Soluta tenetur laborum praesentium quaerat
+            commodi. Mollitia, distinctio nisi repellendus, commodi suscipit,
+            enim illum numquam necessitatibus magni totam beatae!
+          </p>
+
+          <p className="indent-8 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            natus ipsa exercitationem odio, nam deserunt, quas quia eveniet
+            pariatur assumenda rem nostrum dolorum earum debitis eum provident
+            inventore architecto molestiae at, facere rerum sapiente. Placeat
+            optio alias aliquam itaque nesciunt ex adipisci nisi doloribus
+            reprehenderit quibusdam distinctio nobis eaque voluptatem corporis
+            voluptatibus earum, eum laboriosam commodi aliquid sequi voluptate.
+            Ipsam sit quod est eligendi porro sunt fugit magnam sint aut
+            reiciendis, consectetur eum tenetur quis explicabo officiis quo
+            similique maxime veniam. Soluta tenetur laborum praesentium quaerat
+            commodi. Mollitia, distinctio nisi repellendus, commodi suscipit,
+            enim illum numquam necessitatibus magni totam beatae!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Vision;
