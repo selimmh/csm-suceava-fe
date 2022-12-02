85347d4936301945bccffd86be3191973a7b513b
selimmh <selim.mht@gmail.com>
2022-12-02 12:21:43 +0200

    [develop]/add routes file

---
+++ src/routes/ROUTES.ts
@@ -0,0 +1,43 @@
+const ROUTES = {
+  // home
+  home: { path: "/" },
+
+  // details
+  details: { path: "/details" },
+  vision: { path: "/details/vision" },
+  history: { path: "/details/history" },
+  trophies: { path: "/details/trophies" },
+
+  // personnel
+  personnel: { path: "/personnel" },
+  players: { path: "/personnel/players" },
+  coaches: { path: "/personnel/coaches" },
+
+  // seniors
+  seniors: { path: "/seniors" },
+
+  // juvenile
+  juvenile: { path: "/juvenile" },
+  juniors: { path: "/juvenile/juniors" },
+  minivolley: { path: "/juvenile/minivolley" },
+  cadets: { path: "/juvenile/cadets" },
+  youth: { path: "/juvenile/youth" },
+
+  // matches
+  matches: { path: "/matches" },
+
+  // news
+  news: { path: "/news" },
+
+  // partners
+  partners: { path: "/partners" },
+
+  // gallery
+  gallery: { path: "/gallery" },
+
+  // admin
+  admin: { path: "/admin" },
+  login: { path: "/admin/login" },
+};
+
+export default ROUTES;
